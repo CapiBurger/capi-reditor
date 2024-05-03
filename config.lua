bc6b604657caba36024d4075fed2a8c1081be304
CapiBurger <104459014+CapiBurger@users.noreply.github.com>
2024-05-04 00:02:00 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Framework = 'qbcore' -- Por ahora, solo disponible QBCore en caso de usar las notificaciones default, para notificaciones de Origen puedes usarlo de forma standalone. 
+Config.Framework = 'qbcore' -- esx / qbcore / standalone (cambiar en el client.lua)
 
 -- Key Mapping (asignación de teclas)
 Config.Command = 'reditor' -- Comando para abrir el menú de Rockstar Editor
