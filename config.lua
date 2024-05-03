3d5c8f3a414b349c1fe87b7640eadbb284ccd50a
CapiBurger <104459014+CapiBurger@users.noreply.github.com>
2024-05-03 19:31:54 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+-- Key Mapping (asignación de teclas)
+Config.Command = 'reditor' -- Comando para abrir el menú de FPS
+Config.KeyToOpen = 'O' -- Tecla para abrir el menú de FPS
+
+-- Notificación [EXPORT] SE CAMBIARIA EL CONFIG.NOTIFICATION: exports[Config.Notification]:ShowNotification("Se cambiaron los gráficos a bajo", "success") 
+Config.Notification = "origen_notify"
---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'arousa-reditor'
+author 'capi3d'
+description 'ROCKSTAR EDTIOR MENU by ArousaTown'
+version '1.0.0'
+
+client_scripts {
+  "lua/*.lua"
+}
+
+shared_scripts {
+  'config.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+  'html/*.html',
+  'html/css/*.css',
+  'html/js/*.js',
+  'html/fonts/*.ttf',
+  'html/img/png/*.png',
+  'html/img/jpg/*.jpg',
+
+  'html/sounds/*.mp3',
+  'html/sounds/*.wav',
+  'html/sounds/*.ogg',
+}
